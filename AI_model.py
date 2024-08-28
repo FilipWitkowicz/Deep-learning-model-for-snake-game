360512c702cfdf1ed10c3dfc79afc92ad89abe88
FilipWitkowicz <filip.witkowicz1@gmail.com>
2024-08-28 13:26:20 +0200

    loading model

--- AI_model.py
+++ AI_model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import os
+import numpy as np
 
 
 class Linear_QNet(nn.Module):
@@ -16,12 +17,20 @@ class Linear_QNet(nn.Module):
         x = self.linear2(x)
         return x
 
-    # def save(self, file_name="model.pth"):
-    #     model_folder_path = "./model"
-    #     if not os.path.exists(model_folder_path):
-    #         os.makedirs(model_folder_path)
-    #     file_name = os.path.join(model_folder_path, file_name)
-    #     torch.save(self.state_dict(), file_name)
+    def save(self, file_name="model.pth"):
+        model_folder_path = "./model"
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+
+    def load(self, file_name="model.pth"):
+        file_name = os.path.join("model", file_name)
+        if os.path.isfile(file_name):  # Check if the file exists
+            self.load_state_dict(torch.load(file_name, weights_only=True))
+            print(f"Model loaded from {file_name}")
+        else:
+            print(f"No saved model found at {file_name}. Starting fresh.")
 
 
 class QTrainer:
@@ -33,11 +42,11 @@ class QTrainer:
         self.criterion = nn.MSELoss()
 
     def train_step(self, state, action, reward, next_state, done):
-        state = torch.tensor(state, dtype=torch.float)
-        next_state = torch.tensor(next_state, dtype=torch.float)
-        action = torch.tensor(action, dtype=torch.long)
-        reward = torch.tensor(reward, dtype=torch.float)
-        done = torch.tensor(done, dtype=torch.bool)
+        state = torch.tensor(np.array(state), dtype=torch.float)
+        next_state = torch.tensor(np.array(next_state), dtype=torch.float)
+        action = torch.tensor(np.array(action), dtype=torch.long)
+        reward = torch.tensor(np.array(reward), dtype=torch.float)
+        done = torch.tensor(np.array(done), dtype=torch.bool)
 
         if len(state.shape) == 1:
             # (1, x)
